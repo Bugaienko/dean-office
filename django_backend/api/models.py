00e962d61e6a57c3146ca335abdc9e13cd7c404a
Bugaienko <sergiibugaienko@gmail.com>
2022-05-24 12:57:18 +0200

    django api realisated added

--- django_backend/api/models.py
+++ django_backend/api/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+
+    image = models.FileField(upload_to='uploads/images', verbose_name='Фото', null=True, blank=True)
+    name = models.CharField(max_length=100, verbose_name='Имя', null=False, blank=False)
+    email = models.EmailField(verbose_name='Email', max_length=100, null=False, blank=False, unique=True)
+    address = models.TextField(null=False, blank=False, verbose_name='Адрес', max_length=400)
+    phone = models.CharField(max_length=25, blank=True, null=True, verbose_name='Телефон')
+
+    def __str__(self):
+        return str(self.name)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
