e92e2f164d6ecd782793c236b1a05083bbebe0db
Bugaienko <sergiibugaienko@gmail.com>
2022-05-26 11:04:11 +0200

    dev-version

---
+++ react_frontend/src/components/AddStudent.js
@@ -0,0 +1,10 @@
+import  axios from 'axios';
+import React, { useState} from "react";
+import { useHistory } from "react-router-dom";
+
+
+const AddStudents = () => {
+
+};
+
+export default AddStudents;
